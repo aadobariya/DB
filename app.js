c90acc942788c48d8a439755afb147ca4839f573
harsh <harshkarkar333@gmail.com>
2024-06-08 09:20:31 +0530

    DB

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();           
+const morgan = require('morgan');     
+require('dotenv').config();
+const port = process.env.PORT;
+const mongoose = require('mongoose');
+app.use(express.json());
+app.use(morgan("dev"));
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_DB_URL);  
+}
+main()
+.then(()=>console.log('Db is connected........'))
+.catch(err => console.log(err));
+
+app.listen(port,()=>{
+    console.log('Server start at http://localhost:3333');
+});
